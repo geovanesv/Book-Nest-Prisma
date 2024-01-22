24a6eab9a3ee16c0f3ab8dd11bb9ade3754a75fc
Geovane Araujo <geovanesv89@gmail.com>
2024-01-22 15:09:04 -0300

    Criação RestApi Books

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação API Books')
+    .setDescription('RestAPI Books')
+    .setVersion('1.0')
+    .addTag('Books')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
